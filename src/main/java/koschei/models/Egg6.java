efbed80bb3be9caadf28f0a146ec2cce452db917
Eddi777 <Gjhnfkbot1>
2022-04-21 16:24:02 +0300

    Finished

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -1,9 +1,17 @@
 package koschei.models;
 
+import org.springframework.stereotype.Component;
+
 public class Egg6 {
+
+    private Needle7 needle;
+
+    public Egg6 (Needle7 neddle) {
+        this.needle = neddle;
+    }
 
     @Override
     public String toString() {
-        return ", в яйце иголка " + "";
+        return ", в яйце иголка " + needle.toString();
     }
 }
